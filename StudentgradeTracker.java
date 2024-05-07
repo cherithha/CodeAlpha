7c9d36bc8facb2456e63a7f11faa37e7db7152d7
CherithaK <117513961+cherithha@users.noreply.github.com>
2024-05-07 11:45:58 +0530

    Update StudentgradeTracker.java

--- StudentgradeTracker.java
+++ StudentgradeTracker.java
@@ -1,3 +1,11 @@
+
+// TASK - 1
+
+// Student Grade Tracker
+
+// Develop a program that allows a teacher to enter students' grades and compute their average, highest, and lowest scores. You can use arrays or ArrayLists to store the student data.
+
+
 import java.util.Scanner;
 class StudentGraderTracker{
 public static void main(String[] args) {
